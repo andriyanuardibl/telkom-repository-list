1f0e6f5b839d3783f4b6a7bbeb8d77c67fe6ca1f
Andri Yanuardi <andri_yanuardi_bl@yahoo.co.id>
2021-08-25 22:50:13 +0700

    01. Initialize
    01. Initialize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// Github token for user "andriyanuardibl (andri_yanuardi_bl@yahoo.co.id)"
+// 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
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <h1>Andri Yanuardi</h1>
+            </>
+        )
+    }
+}
+
+export default App
